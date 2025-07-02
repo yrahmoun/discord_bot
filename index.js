b3ed2b16a6a5518fd82fa7a938be3a3cb8038f4c
“Rahmouni <rahmouniyasser77@gmail.com>
2025-07-02 16:21:04 +0200

    add port for render

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Bot is running"));
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 const client = new Client({
   intents: [
